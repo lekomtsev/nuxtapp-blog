5f2ffc904a45a9f4a5497fa3db7389ad23788f05
lekomtsev <mike@jsmaster.ru>
2020-02-29 15:03:41 +0400

    Главная страница

---
+++ components/main/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="post">
+    <p>Post 111</p>
+  </article>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.post {
+  height: 200px;
+  border: 1px solid #000;
+  margin-bottom: 2rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-    <p>123 Body</p>
-  </div>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10">
+      <app-post
+        v-for="post in 3"
+        :key="post"
+      />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {
+import AppPost from '@/components/main/Post'
 
+export default {
+  components: {
+    AppPost
+  },
+  head () {
+    return {
+      title: 'Главная'
+    }
+  }
 }
 </script>
