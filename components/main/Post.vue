da8da11344838ef6e8aafd77f090c594e14ecb9c
lekomtsev <mike@jsmaster.ru>
2020-02-29 15:43:32 +0400

    Компонент пост

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,17 +1,62 @@
 <template>
-  <article class="post">
-    <p>Post 111</p>
-  </article>
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header class="post__header" slot="header">
+      <h3>Post title</h3>
+      <small>
+        <i class="el-icon-time" />
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post__body">
+      <img class="post__image" src="https://www.1obl.ru/upload/iblock/3d4/berlin.png" alt="alt">
+    </div>
+    <footer class="post__footer">
+      <el-button round @click="openPost">
+        Открыть статью
+      </el-button>
+      <span>
+        <i class="el-icon-message" />
+        12
+      </span>
+    </footer>
+  </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    openPost () {
+      // console.log('open post')
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .post {
-  height: 200px;
-  border: 1px solid #000;
+  // height: 350px;
   margin-bottom: 2rem;
+  overflow: hidden;
+
+  img {
+    max-width: 100%;
+  }
+}
+
+.post__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.post__footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px;
 }
 </style>
