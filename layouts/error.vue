b6d3e4126f1f2357448347119683877d464ec27e
lekomtsev <mike@jsmaster.ru>
2020-02-29 16:51:34 +0400

    Обработка ошибки роутов

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="error">
+    <div class="error__wrapper">
+      <div class="error__title">
+        Ошибка 404
+      </div>
+      <nuxt-link to="/">
+        На главную
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.error {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.error__title {
+  font-size: 30px;
+}
+</style>
