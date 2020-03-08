cfb5c3814861cffebdc46aec475891b06f5c4ecc
lekomtsev <mike@jsmaster.ru>
2020-03-08 15:48:04 +0400

    Создание структуры страниц

---
+++ layouts/admin.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container :class="'admin-page'">
+    <el-aside width="200px">
+      Aside
+    </el-aside>
+    <el-container>
+      <el-main>
+        <nuxt />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.admin-page {
+  height: 100vh
+}
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Welcome to admin page</p>
+</template>
+
+<script>
+export default {
+  layout: 'admin'
+}
+</script>
