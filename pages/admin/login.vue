acd3ca11e323f4a879389343ac98a7d2b4172b9b
lekomtsev <mike@jsmaster.ru>
2020-04-06 16:24:22 +0400

    Защита роутов с Middleware

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -55,6 +55,15 @@ export default {
       }
     }
   },
+
+  mounted () {
+    const { message } = this.$route.query
+
+    if (message === 'login') {
+      this.$message.info('Нужно войти в систему')
+    }
+  },
+
   methods: {
     onSubmit () {
       this.$refs.form.validate(async (valid) => {
