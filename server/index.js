a067dc3177e842a876d2e82236e3ddb5679e05ef
lekomtsev <mike@jsmaster.ru>
2020-04-07 21:08:24 +0400

    change server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-const express = require('express')
+// const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
-const app = express()
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
+const app = require('./app')
+
 config.dev = process.env.NODE_ENV !== 'production'
 
 async function start () {
@@ -24,10 +25,11 @@ async function start () {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
+  app.listen(port, host, () => {
+    consola.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true
+    })
   })
 }
 start()
