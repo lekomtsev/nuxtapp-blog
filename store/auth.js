c04ff9f1632827895b5df6f0bb76ff9496aaebe0
lekomtsev <mike@jsmaster.ru>
2020-04-08 17:10:52 +0400

    Поддержание сессии с токеном

--- store/auth.js
+++ store/auth.js
@@ -1,3 +1,7 @@
+import Cookie from 'cookie'
+import Cookies from 'js-cookie'
+import jwtDecode from 'jwt-decode'
+
 export const state = () => ({
   token: null
 })
@@ -26,11 +30,13 @@ export const actions = {
   setToken ({ commit }, token) {
     this.$axios.setToken(token, 'Bearer')
     commit('setToken', token)
+    Cookies.set('jwt-token', token)
   },
 
   logout ({ commit }) {
     this.$axios.setToken(false)
     commit('clearToken')
+    Cookies.remove('jwt-token')
   },
 
   async createUser ({ commit }, formData) {
@@ -40,10 +46,36 @@ export const actions = {
       commit('setError', error, { root: true })
       throw error
     }
+  },
+
+  autoLogin ({ dispatch }) {
+    const cookieStr = process.browser
+      ? document.cookie
+      : this.app.context.req.headers.cookie
+
+    const cookies = Cookie.parse(cookieStr || '') || {}
+    const token = cookies['jwt-token']
+
+    if (isJWTValid(token)) {
+      dispatch('setToken', token)
+    } else {
+      dispatch('logout')
+    }
   }
 }
 
 export const getters = {
   isAuthenticated: state => Boolean(state.token),
   token: token => state.token
+}
+
+function isJWTValid (token) {
+  if (!token) {
+    return false
+  }
+
+  const jwtData = jwtDecode(token) || {}
+  const expires = jwtData.exp || 0
+
+  return (new Date().getTime() / 1000) < expires
 }
