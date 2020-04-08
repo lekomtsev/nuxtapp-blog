45b55550db33d605c18f40c7ef32798e203727de
lekomtsev <mike@jsmaster.ru>
2020-04-08 15:28:48 +0400

    Логин и обработка ошибок

--- store/auth.js
+++ store/auth.js
@@ -1,5 +1,5 @@
 export const state = () => ({
-  token: true
+  token: null
 })
 
 export const mutations = {
@@ -16,7 +16,6 @@ export const actions = {
   async login ({ commit, dispatch }, formData) {
     try {
       const { token } = await this.$axios.$post('/api/auth/admin/login', formData)
-      console.log(token, 'token')
       dispatch('setToken', token)
     } catch (error) {
       commit('setError', error, { root: true })
