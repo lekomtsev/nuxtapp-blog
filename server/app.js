39ea542a10304818e1ea4e15fb46599a32946e95
lekomtsev <mike@jsmaster.ru>
2020-04-08 10:42:25 +0400

    Подключение MongoDB

--- server/app.js
+++ server/app.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const keys = require('./keys')
 const app = express()
+
+console.log(keys.MONGO_URI)
+
+mongoose.connect(keys.MONGO_URI)
+  .then(() => console.log('MongoDb connected...'))
+  .catch(error => console.error(error))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 module.exports = app
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://admin:<password>@test-nuxt-blog-2epp6.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,5 @@
-module.export = {
+// mongodb+srv://<username>:<password>@test-nuxt-blog-2epp6.mongodb.net/test?retryWrites=true&w=majority
 
+module.exports = {
+  MONGO_URI: 'mongodb+srv://admin:admin@test-nuxt-blog-2epp6.mongodb.net/test?retryWrites=true&w=majority'
 }
